891576a83cf18ec30ed86d20d138cd52e6eaf89d
KorKompot291 <kotkompot291@gmail.com>
2026-03-09 13:35:40 +0300

    add online chat and edit some text

--- js/config.js
+++ js/config.js
@@ -2,10 +2,10 @@
 // Edit this file to point the website chat to your n8n webhooks.
 window.N8N_CONFIG = {
   // POST: send a message from web chat to n8n
-  sendUrl: 'https://it-atelier-bot-kotkompot291.amvera.io/webhook/tour-web-send',
+  sendUrl: 'https://autoreplyforkhmersoul-production.up.railway.app/chat',
 
   // GET: poll for new messages from n8n
-  pollUrl: 'https://it-atelier-bot-kotkompot291.amvera.io/webhook/webchat/poll',
+  pollUrl: 'https://autoreplyforkhmersoul-production.up.railway.app/poll',
 
   // Optional: how often to poll (ms)
   pollIntervalMs: 2500
